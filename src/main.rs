150a60a4bbb037ea88b8f153d428543f0a9537b0
Gregory Croisdale <gtcroisdale1@gmail.com>
2025-09-22 10:42:38 -0400

    fix SSG

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,29 @@ mod components;
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const TAILWIND_CSS: Asset = asset!("/assets/GENERATED_tailwind.css");
 
+// https://dioxuslabs.com/learn/0.6/guides/fullstack/static_site_generation/#setting-up-the-serveconfig
 fn main() {
-    dioxus::launch(App);
+    dioxus::LaunchBuilder::new()
+        .with_cfg(server_only! {
+            ServeConfig::builder()
+                // Enable incremental rendering
+                .incremental(
+                    IncrementalRendererConfig::new()
+                        // Store static files in the public directory where other static assets like wasm are stored
+                        .static_dir(
+                            std::env::current_exe()
+                                .unwrap()
+                                .parent()
+                                .unwrap()
+                                .join("public")
+                        )
+                        // Don't clear the public folder on every build. The public folder has other files including the wasm
+                        // binary and static assets required for the app to run
+                        .clear_cache(false)
+                )
+                .enable_out_of_order_streaming()
+        })
+        .launch(App);
 }
 
 #[component]
